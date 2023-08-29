c597bcfa85030cb6dbd912445fb40a2a5d5748fc
Cole Gerdemann <corvettecole@gmail.com>
2023-08-29 15:45:41 -0400

    begin implementation of SharedDatabase. Pivot to using unnamed semaphores for per-entry locking

--- assignment_1/common/SharedDatabase.cpp
+++ assignment_1/common/SharedDatabase.cpp
@@ -1,17 +1,75 @@
 #include "SharedDatabase.h"
 
-template <class T>
+#include <time.h>
+
+template <typename T>
 SharedDatabase<T>::SharedDatabase(const std::string &id,
                                   std::string &password) {
   // TODO generate key_t using ftok() from id
   // Setup shared memory segment(s) to share:
-  // - database (objects stored in shared memory as an array?)
-  // - database password
-  // - either use named semaphores or somehow share a shared_mutex
+  // - database
+  // - database metadata
+
+  // Also need to set the named semaphore for tracking processes using the
+  // database
 
   // Need to handle creating this all if it doesn't exist, or opening it if it
   // does exist. So the first thing to do is check if a particular shared memory
   // segment exists whose key is derived from the id as mentioned above.
+}
 
+template <typename T>
+size_t SharedDatabase<T>::getSize() const {
+  return this->size;
+}
+template <typename T>
+size_t SharedDatabase<T>::getMaxSize() const {
+  return this->maxSize;
+}
 
+template <typename T>
+T SharedDatabase<T>::get(size_t index) const {
+  auto lock = lockEntry(index);
+  // Return a copy of the data
+  return this->entries[index].data;
+}
+
+template <typename T>
+std::shared_ptr<T> SharedDatabase<T>::at(size_t index) {
+  auto lock = lockEntry(index);
+  // Return a shared pointer to the data, with a custom deleter that unlocks
+  // the entry
+  return std::make_shared<T>(&this->entries[index].data,
+                             [lock](T *data) { lock.reset(); });
+}
+
+template <typename T>
+std::shared_ptr<T> SharedDatabase<T>::operator[](std::size_t index) {
+  return at(index);
+}
+
+template <typename T>
+std::shared_ptr<sem_t> SharedDatabase<T>::lockEntry(size_t index) const {
+  // check if index is within bounds
+  if (index >= this->size) {
+    throw std::out_of_range("Index out of bounds");
+  }
+
+  // Get a lock on the database entry. Da a sem_timedwait() on it and
+  // throw an exception if it times out.
+  timespec timeout{LOCK_TIMEOUT.count()};
+  auto lock = this->entries[index].lock;
+  switch (sem_timedwait(lock, &timeout)) {
+    case 0:
+      break;
+    case ETIMEDOUT:
+      throw std::runtime_error(
+          "Failed to acquire exclusive lock within timeout");
+    default:
+      throw std::runtime_error("Failed to acquire exclusive lock");
+  }
+
+  // Return a shared pointer to the semaphore, with a custom deleter that
+  // unlocks the semaphore
+  return std::make_shared<sem_t>(lock, [](sem_t *lock) { sem_post(lock); });
 }
--- assignment_1/common/SharedDatabase.h
+++ assignment_1/common/SharedDatabase.h
@@ -8,16 +8,48 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#include <chrono>
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
-#include "StudentInfo.h"
+// using chrono types
+using namespace std::chrono_literals;
 
-template <class T>
+constexpr std::chrono::seconds LOCK_TIMEOUT = 5s;
+
+// Represents the metadata of the database. Contains the version of the database
+// and a hash of the password. This is stored in shared memory and processes can
+// use it to determine if they should mount the database shared memory segment
+// as read-only or read-write.
+struct DatabaseMetadata {
+  sem_t lock;
+  uint32_t version;
+  std::size_t passwordHash;
+};
+
+template <typename T>
+// Represents a single entry in the database. Contains the data and a semaphore
+// for locking the entry.
+struct DatabaseEntry {
+  sem_t lock;
+  T data;
+};
+
+template <typename T>
+// Represents the database. Contains metadata about the database; a hash of the
+// password, and an array of entries.
+struct Database {
+  size_t size;
+  size_t maxSize;
+  DatabaseEntry<T> entries[];
+};
+
+template <typename T>
 // Implements a database of objects, stored in shared memory, that can be
 // accessed by multiple processes concurrently.
-class SharedDatabase {
+class SharedDatabase : private Database<T> {
  public:
   // Creates a new shared database with the given identifier. If a database
   // with the same identifier already exists, and the password matches, the
@@ -29,9 +61,9 @@ class SharedDatabase {
   // the database, it will be cleaned up from shared memory as well.
   ~SharedDatabase();
 
-  [[nodiscard]] size_t size() const;
+  [[nodiscard]] size_t getSize() const;
 
-  [[nodiscard]] size_t max_size() const;
+  [[nodiscard]] size_t getMaxSize() const;
 
   // Returns a copy of the element at the given index.
   [[nodiscard]] T get(size_t index) const;
@@ -63,8 +95,13 @@ class SharedDatabase {
   // Named semaphore for tracking the number of processes using the database.
   sem_t *shared_;
 
-  // Named semaphore for locking the database.
-  sem_t *exclusive_;
+  // Database metadata
+  DatabaseMetadata *metadata_;
+
+  // Gets a semaphore associated with the given index. Automatically unlocks the
+  // semaphore when the returned shared pointer is destroyed.
+  [[nodiscard]] std::shared_ptr<sem_t> lockEntry(size_t index) const;
 };
+
 
 #endif  // ASSIGNMENT_1_SHAREDDATABASE_H
